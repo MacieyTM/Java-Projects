afe62592245d9548ca7684007d12591f37be7966
Maciej Grochowski <95743795+MacieyTM@users.noreply.github.com>
2022-03-25 17:43:24 +0100

    Add files via upload

---
+++ InnerAndAbstractClassesAndInterfaces/AbstractClasses/Abstraction/src/grochowski/maciej/CanFly.java
@@ -0,0 +1,8 @@
+package grochowski.maciej;
+
+/**
+ * Created by dev on 9/10/2015.
+ */
+public interface CanFly {
+    void fly();
+}
---
+++ JavaGenerics/Genericsv2/GenericsPart2/src/grochowski/maciej/FootballPlayer.java
@@ -0,0 +1,11 @@
+package grochowski.maciej;
+
+/**
+ * Created by dev on 17/10/2015.
+ */
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
